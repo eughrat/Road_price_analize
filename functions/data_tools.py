0977ca06139f28364e726ac55167e0565bedc0bc
eughrat <michal.piernicki@gmail.com>
2022-04-24 22:43:23 +0200

    remove hardcode elements from functions, make them more readable

--- functions/data_tools.py
+++ functions/data_tools.py
@@ -2,37 +2,46 @@ import numpy as np
 import pandas as pd
 
 
-def get_element(df, word:str):
-  
-    road_elem_df = df[df['Elementy_rozliczeniowe'].str.contains(word)]
-    road_elem_df = road_elem_df.groupby('Droga').agg({'Ilosc': 'sum',
-                                  'Dlugosc_drogi':'max'})
+# class PdfDataFrame():
     
+#     def _init_(self):
+#         self.df = df
+
+def get_element(df, word:str, col_to_group='Elementy_rozliczeniowe', col_groupby= 'Droga', agg_dict={'Ilosc': 'sum','Dlugosc_drogi':'max'}):
+
+    road_elem_df = df[df[col_to_group].str.contains(word)]
+    road_elem_df = road_elem_df.groupby(col_groupby).agg(agg_dict)
+
     return road_elem_df
 
-def get_multiple_elem_method_or(df,*args):
-    
+def get_multiple_elem_method_or(df, *args, col_to_group='Elementy_rozliczeniowe', col_groupby= 'Droga', agg_dict={'Ilosc': 'sum','Dlugosc_drogi':'max'}):
+
     search = list(args)
-    road_elem_df = df[df['Elementy_rozliczeniowe'].str.contains('|'.join(search))]
-    road_elem_df = road_elem_df.groupby('Droga').agg({'Ilosc': 'sum',
-                                                      'Dlugosc_drogi':'max'})
-    
+    road_elem_df = df[df[col_to_group].str.contains('|'.join(search))]
+    road_elem_df = road_elem_df.groupby(col_groupby).agg(agg_dict)
+
     return road_elem_df
 
-def get_multiple_elem_method_and(df,*args):
-    
+def get_multiple_elem_method_and(df, *args, col_to_group='Elementy_rozliczeniowe', col_groupby= 'Droga', agg_dict={'Ilosc': 'sum','Dlugosc_drogi':'max'},):
+
     base = r'^{}'
     expr = '(?=.*{})'
     words = list(args)
     search = base.format(''.join(expr.format(w) for w in words))
-    road_elem_df = df[df['Elementy_rozliczeniowe'].str.contains(search)]
-    road_elem_df = road_elem_df.groupby('Droga').agg({'Ilosc': 'sum',
-                                                      'Dlugosc_drogi':'max'})
-    
+    road_elem_df = df[df[col_to_group].str.contains(search)]
+    road_elem_df = road_elem_df.groupby(col_groupby).agg(agg_dict)
+
     return road_elem_df
 
 def col_per_km(df, col_name:str, col_origin:str):
-    
+
     df[col_name] = df[col_origin] / df['Dlugosc_drogi']
-    
+
     return df
+
+
+def map_col_type(df, names:list):
+    
+    for i in range(names):
+         type = set(df[names[i]].map(type))
+         return print(f"Column {names[i]} type: {type[0]}")
--- functions/pdf_tools.py
+++ functions/pdf_tools.py
@@ -4,17 +4,15 @@ import pandas as pd
 import glob
 from difflib import get_close_matches
 
-
-def pdf_reader(path:str):
+def read_pdfs(path:str,new_col_names =['Lp','CPV','Numer_Specyfikacji_Technicznej','Elementy_rozliczeniowe','Jednostka','Ilosc','Cena_jedn','Wartosc_calkowita','Droga','Rok','Kategoria']):
 
     pdf_path_list = []
     all_reports_list=[]
-    road_length_list=[]
 
 #   Import pdf path to path_list to extract them to the df in the next step
     for file in glob.glob(path):
         pdf_path_list.append(file)
-        
+
 #   Read all pdf files one after another
     for path in pdf_path_list:
         road_data = cam.read_pdf(path,pages='all', flavor='lattice')
@@ -23,55 +21,42 @@ def pdf_reader(path:str):
         for page, pdf_table in enumerate(road_data):
             report = road_data[page].df
             report_cl = report.iloc[1:]
-            
+
 #       Setting new column names
-            report_cl.columns = 'Lp CPV Numer_Specyfikacji_Technicznej Elementy_rozliczeniowe Jednostka Ilosc Cena_jedn Wartosc_calkowita Droga Rok Kategoria'.split() 
-     
+            report_cl.columns = new_col_names
             all_reports_list.append(report_cl)
-            
+
 #   Concate all single dfs to one DataFrame
     all_reports_df = pd.concat(all_reports_list)
-    
+
     return all_reports_df
 
-################################################################################################################
+def clean_pdf(df,words_to_replace={'\n': '','': np.nan},col_to_drop=['Lp','CPV','Numer_Specyfikacji_Technicznej']):
 
-def pdf_cleaner(df):
-        
     #Replace all blank cells with NaN
     df.ffill(axis=0,inplace=True)
-    df.replace({'\n': '',
-               '': np.nan}, regex=True, inplace=True)
-        
+    df.replace(words_to_replace, regex=True, inplace=True)
+
     # Columns "Lp CPV Numer_Specyfikacji_Technicznej" are not essential to road costs analysis so its ok to drop them    
-    df.drop(['Lp','CPV','Numer_Specyfikacji_Technicznej'], axis=1, inplace=True)
-    
+    df.drop(col_to_drop, axis=1, inplace=True)
+
     # Now we can see that our dataframe have NaN cells only in dexcription rows, so we can drop rows with NaN.
     df.dropna(inplace=True)
-        
+
     return df
 
-################################################################################################################
+def match_category(row, cor_cat_list=['ROBOTY PRZYGOTOWAWCZE','OZNAKOWANIA DROG','ZIELEN DROGOWA','PODBUDOWY','NAWIERZCHNIE','ROBOTY WYKONCZENIOWE','ELEMENTY ULIC','WARUNKI OGOLNE','ROBOTY ZIEMNE','ODWODNIENIE','ODWODNIENIE KORPUSU DROGOWEGO','INNE ROBOTY']):
 
-def match_category(row):
 #   List of accepted categories of construction work
-    correct_cat = ['ROBOTY PRZYGOTOWAWCZE',
-                    'OZNAKOWANIA DROG',
-                    'ZIELEN DROGOWA',
-                    'PODBUDOWY',
-                    'NAWIERZCHNIE',
-                    'ROBOTY WYKONCZENIOWE',
-                    'ELEMENTY ULIC',
-                    'WARUNKI OGOLNE',
-                    'ROBOTY ZIEMNE',
-                    'ODWODNIENIE',
-                    'ODWODNIENIE KORPUSU DROGOWEGO',
-                    'INNE ROBOTY']
-    
+    correct_cat = cor_cat_list
+
 #   Return a list of the best “good enough” matches
     match = get_close_matches(row, correct_cat, n=1,cutoff=0.3)
-    
+
     return match[0] if match else ''
+
+       
+
 
         
         
